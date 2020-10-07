4b149a544266dfd30c0170a42b2795f6d6a145e2
Zeyuan Zheng <zzheng26@hawk.iit.edu>
2020-10-07 15:29:49 -0500

    init

---
+++ src/test/java/com/BestDealStore/BestDealStore_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BestDealStore.BestDealStore_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
